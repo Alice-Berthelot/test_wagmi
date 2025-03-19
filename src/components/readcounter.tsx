996a298835843ec8b36201fe96afe18dc3bcb2fb
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-03-19 13:25:12 +0100

    tried realtime update

--- src/components/readcounter.tsx
+++ src/components/readcounter.tsx
@@ -1,17 +1,29 @@
-import { useReadContract } from "wagmi";
+import { useReadContract, useWatchContractEvent } from "wagmi";
 import { wagmiContractConfig } from "../contracts";
+// import { useState } from "react";
 
 export default function ReadCounter() {
-  const { data: count } = useReadContract({
+  // const [count, setCount] = useState<string>("");
+
+  const { data: value } = useReadContract({
     address: wagmiContractConfig.address,
     abi: wagmiContractConfig.abi,
     functionName: "count",
   });
 
+  // useWatchContractEvent({
+  //   address: wagmiContractConfig.address,
+  //   abi: wagmiContractConfig.abi,
+  //   eventName: 'Incremented',
+  //   onLogs: (logs) => {
+  //     setCount((logs[0].args?.value as BigInt).toString());
+  //   },
+  // })
+
   return (
     <div className="flex flex-col justify-center items-center mx-auto border border-neutral-900 p-2 w-50 rounded-lg text-lg mb-2">
       <p className="font-medium">Realtime counter:</p>
-      <p className="font-bold text-xl"> {count?.toString()}</p>
+      <p className="font-bold text-xl">{value?.toString()}</p>
     </div>
   );
 }
